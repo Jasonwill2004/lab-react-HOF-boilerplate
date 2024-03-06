b6c057eb811406252c040feca8a5967bbab45492
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-06 10:53:31 +0530

    done

---
+++ react_hof/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HigherOrderComponent from "./components/HigherOderFunction";
+
+
+function App() {
+  return (
+    <>
+      <HigherOrderComponent/>
+    </>
+  );
+}
+
+export default App;
